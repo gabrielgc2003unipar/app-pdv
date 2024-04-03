a6010d91cdf9f5920fafebff77f17ffa62c94e32
Gabriel Garcia Couto <127454621+gabrielgc2003unipar@users.noreply.github.com>
2024-04-03 19:36:22 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/app/pdv/AppPdv.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app.pdv;
+
+/**
+ *
+ * @author gabri
+ */
+public class AppPdv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
